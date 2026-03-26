ce310792b5eb4a7e629be3023c58878fe48861d7
natabatistadossantos <nata_santos18@hotmail.com>
2026-03-26 18:17:36 -0300

    apaguei

--- JogoPenalti.java
+++ JogoPenalti.java
@@ -51,11 +51,11 @@ public class JogoPenalti {
         System.out.println(goleiro + " fez " + defesas + " defesa(s)");
 
         if (gols > defesas) {
-            System.out.println("🏆 " + atacante + " VENCEU!");
+            System.out.println( atacante + " VENCEU!");
         } else if (defesas > gols) {
-            System.out.println("🏆 " + goleiro + " VENCEU!");
+            System.out.println( goleiro + " VENCEU!");
         } else {
-            System.out.println("🤝 Empate!");
+            System.out.println(" Empate!");
         }
 
         scanner.close();
